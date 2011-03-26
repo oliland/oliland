49ee6cebb9b34f47b865846a5d8b165dc64b492e
Oli Kingshott <oli@oliland.net>
2011-03-26 17:56:00 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oliland::Application.config.secret_token = '29a46dc8026131305ed05edcf51960d7ecf12c5560492545dba3dbf089f075175d51e4df81b96d90dc092ad28896c23de5082e7af5501238ef4e727bc9baeea8'
